7d7a92ea8d392570a7cabbfad6380f1d4b9793c3
Kawou-dev <levieux426@gmail.com>
2025-05-30 16:42:41 +0100

    changing profile picture

--- app/components/Author.jsx
+++ app/components/Author.jsx
@@ -8,26 +8,10 @@ const Author = () => {
     <div className=' w-[70%] mx-auto my-10  '>
 
            <div className='flex flex-col gap-5 justify-center'>
-              <div className="p-5 border shadow-md rounded text-gray-500">
-            <div className="flex items-center">
-                <Image loading='lazy' className="w-20 h-20 shadow-md rounded-full mr-3" src="/images/bhoye.jpg" alt="jane" />
-                <div className="text-sm">
-                    <a href="#"
-                        className="font-medium leading-none text-gray-900 hover:text-indigo-600 transition duration-500 ease-in-out">
-                       Balde Mamadou Bhoye
-                    </a>
-                    <p>Etudiant en Informatique</p>
-                </div>
-            </div>
 
-            <p className="mt-2 text-sm text-gray-900">
-    Je développe mes compétences en informatique avec l’objectif de résoudre des problèmes réels et d’avoir un impact positif dans le secteur technologique.
-                </p>
-
-             </div>
-                 <div className="p-5 border shadow-md rounded text-gray-500">
-            <div className="flex items-center">
-                <Image loading='lazy' className="w-20 h-20 rounded-full shadow-md mr-3" src="/images/admin_photo.png" alt="jane" />
+                <div className="p-5 border shadow-md rounded text-gray-500">
+                 <div className="flex items-center">
+                <Image loading='lazy' className="w-20 h-20 rounded-full shadow-md mr-3" src="/images/admin_p.jpg" alt="jane" />
                 <div className="text-sm">
                     <a href="#"
                         className="font-medium leading-none text-gray-900 hover:text-indigo-600 transition duration-500 ease-in-out">
@@ -42,6 +26,26 @@ const Author = () => {
             </p>
 
              </div>
+
+           
+              <div className="p-5 border shadow-md rounded text-gray-500">
+            <div className="flex items-center">
+                <Image loading='lazy' className="w-20 h-20 shadow-md rounded-full mr-3" src="/images/bhoye.jpg" alt="jane" />
+                <div className="text-sm">
+                    <a href="#"
+                        className="font-medium leading-none text-gray-900 hover:text-indigo-600 transition duration-500 ease-in-out">
+                       Balde Mamadou Bhoye
+                    </a>
+                    <p>Etudiant en Informatique</p>
+                </div>
+            </div>
+
+            <p className="mt-2 text-sm text-gray-900">
+             Je développe mes compétences en informatique avec l’objectif de résoudre des problèmes réels et d’avoir un impact positif dans le secteur technologique.
+                </p>
+
+             </div>
+             
 
            </div>
     </div>
